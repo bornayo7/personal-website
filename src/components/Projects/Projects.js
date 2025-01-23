a5dc3275aa186d68291a79452826a5486733ffc2
bornayo7 <yashbaruah@hotmail.com>
2025-01-23 17:54:13 -0600

    Adding another project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import leetcode from "../../Assets/Projects/leetcode.png";
 import AIS from "../../Assets/Projects/ais_utd_logo.png";
 import website from "../../Assets/Projects/website.png";
+import cometpark from "../../Assets/Projects/cometpark.png";
 import About from "../About/About";
 
 function Projects() {
@@ -17,24 +18,25 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-        <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={AIS}
               isBlog={false}
               title="AI Music Genre Classification Project"
               description="Currently developing a machine learning model using a Convolutional Neural Network (CNN) to classify music genres based on audio spectrograms. Utilizing Python, TensorFlow, Librosa, and pandas for ongoing data preprocessing and model training. Building a full-stack web application with React and Node.js to present classification results, allowing users to interact with the system and upload their own music for real-time classification."
+              ghLink="https://github.com/ly-sona/music-genre-classification"
             />
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={website}
+              imgPath={cometpark}
               isBlog={false}
-              title="This Website"
-              description="Built a personal portfolio website using React and Node.js to showcase projects and skills. 
-              Implemented responsive design and dynamic routing to allow easy navigation. 
-              Integrated GitHub for version control and deployed the website using modern web development frameworks."
-              ghLink="https://github.com/bornayo7/personal-website"
-              demoLink="https://bornayo7.github.io/personal-website/"
+              title="CometPark"
+              description="Collaborated with a team to develop CometPark, a TypeScript-based web application for tracking and analyzing parking lot operations at UTD. 
+    Utilized React for the frontend, Node.js and Express.js for the backend, and MongoDB for data storage. 
+    Implemented data updates using Pinata to ensure secure and efficient data management."
+              ghLink="https://github.com/yourusername/CometPark"
+              demoLink="https://cometpark.org"
             />
           </Col>
 
@@ -49,12 +51,22 @@ function Projects() {
               ghLink="https://github.com/bornayo7/Leetcode-Daily-Bot"
             />
           </Col>
-
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={website}
+              isBlog={false}
+              title="This Website"
+              description="Built a personal portfolio website using React and Node.js to showcase projects and skills. 
+              Implemented responsive design and dynamic routing to allow easy navigation. 
+              Integrated GitHub for version control and deployed the website using modern web development frameworks."
+              ghLink="https://github.com/bornayo7/personal-website"
+              demoLink="https://bornayo7.github.io/personal-website/"
+            />
+          </Col>
         </Row>
         <About></About>
       </Container>
     </Container>
-    
   );
 }
 
